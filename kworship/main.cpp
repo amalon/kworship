4db1aa8f215e28f50f3a570530bf1c91c0f61a20
James Hogan <james@albanarts.com>
2008-09-20 23:17:58 +0100

    Added COPYING and legal notice to top of main.cpp, also updated kde about data

--- kworship/main.cpp
+++ kworship/main.cpp
@@ -1,23 +1,21 @@
 /***************************************************************************
- *   Copyright (C) 2008 by James Hogan   *
- *   james@albanarts.com   *
+ *   Copyright 2008 James Hogan <james@albanarts.com>                      *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
+ *   This file is part of KWorship.                                        *
+ *                                                                         *
+ *   KWorship is free software: you can redistribute it and/or modify      *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation, either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
+ *   KWorship is distributed in the hope that it will be useful,           *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *   along with KWorship.  If not, see <http://www.gnu.org/licenses/>.     *
  ***************************************************************************/
-
 
 #include "kworship.h"
 
@@ -27,15 +25,16 @@
 #include <kconfigdialogmanager.h>
 #include <KDE/KLocale>
 
-static const char description[] =
-    I18N_NOOP("A KDE 4 Application");
-
-static const char version[] = "0.1";
-
 int main(int argc, char **argv)
 {
-    KAboutData about("kworship", 0, ki18n("kworship"), version, ki18n(description),
-                     KAboutData::License_GPL, ki18n("(C) 2008 James Hogan"), KLocalizedString(), 0, "james@albanarts.com");
+    KAboutData about("kworship", 0,
+                     ki18n("KWorship"), "0.1",
+                     ki18n("Free/Open source church worship & presentation software"),
+                     KAboutData::License_GPL_V3,
+                     ki18n("(C) 2008 James Hogan"),
+                     KLocalizedString(),
+                     "http://kworship.org",
+                     "bugs@kworship.org");
     about.addAuthor( ki18n("James Hogan"), KLocalizedString(), "james@albanarts.com" );
     KCmdLineArgs::init(argc, argv, &about);
 
