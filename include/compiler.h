a9f1f15811b72aacd309acd71fddc5401a6059f9
James Hogan <james@albanarts.com>
2008-10-02 03:59:50 +0100

    Change license to GPL 2 (or later)

--- include/compiler.h
+++ include/compiler.h
@@ -1,11 +1,10 @@
 /***************************************************************************
- *   Copyright 2008 James Hogan <james@albanarts.com>                      *
- *                                                                         *
  *   This file is part of KWorship.                                        *
+ *   Copyright 2008 James Hogan <james@albanarts.com>                      *
  *                                                                         *
  *   KWorship is free software: you can redistribute it and/or modify      *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation, either version 3 of the License, or     *
+ *   the Free Software Foundation, either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
  *   KWorship is distributed in the hope that it will be useful,           *
@@ -14,7 +13,8 @@
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
- *   along with KWorship.  If not, see <http://www.gnu.org/licenses/>.     *
+ *   along with KWorship.  If not, write to the Free Software Foundation,  *
+ *   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.   *
  ***************************************************************************/
 
 #ifndef _compiler_h_
--- kworship/kworship.h
+++ kworship/kworship.h
@@ -4,7 +4,7 @@
  *                                                                         *
  *   KWorship is free software: you can redistribute it and/or modify      *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation, either version 3 of the License, or     *
+ *   the Free Software Foundation, either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
  *   KWorship is distributed in the hope that it will be useful,           *
@@ -13,7 +13,8 @@
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
- *   along with KWorship.  If not, see <http://www.gnu.org/licenses/>.     *
+ *   along with KWorship.  If not, write to the Free Software Foundation,  *
+ *   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.   *
  ***************************************************************************/
 
 #ifndef _kworship_h_
